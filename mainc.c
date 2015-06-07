63674cb10181ec2c4cf014bde839e406c30f8923
wolfelin <wolfelin@uranie>
2015-06-07 10:23:53 +0200

    amorce main test

---
+++ mainc.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib>
+#include <ctype.h>
+#include "libfts"
+
+int	test_bzero();
+int	test_strcat();
+int	test_isalpha();
+int	test_isdigit();
+int	test_isalnum();
+int	test_isascii();
+int	test_isprint();
+int	test_toupper();
+int	test_tolower();
+int	test_strlen();
+int	test_memcpy();
+int	test_memset();
+int	test_puts();
+int	test_strdup();
+
+int	main()
+{
+  test_bzero();
+  getchar();
+  test_strcat();
+  getchar();
+  test_isalpha();
+  getchar();
+  test_isdigit();
+  getchar();
+  test_isalnum();
+  getchar();
+  test_isascii();
+  getchar();
+  test_isprint();
+  getchar();
+  test_toupper();
+  getchar();
+  test_tolower();
+  getchar();
+  test_strlen();
+  getchar();
+  test_memcpy();
+  getchar();
+  test_memset();
+  getchar();
+  test_puts();
+  getchar();
+  test_strdup();
+  getchar();
+  return (0)
+}
+
+int	test_bzero()
+{
+  return (0);
+}
+
+int	test_strcat()
+{
+  return (0);
+}
+
+int	test_isalpha()
+{
+  
+  return (0);
+}
+  
+int	test_isdigit()
+{
+  return (0);
+}
+  
+int	test_isalnum()
+{
+  return (0);
+}
+
+int	test_isascii()
+{
+  return (0);
+}
+
+int	test_isprint()
+{
+  return (0);
+}
+
+int	test_toupper()
+{
+  return (0);
+}
+
+int	test_tolower()
+{
+  return (0);
+}
+
+int	test_strlen()
+{
+  return (0);
+}
+
+int	test_memcpy()
+{
+  return (0);
+}
+
+int	test_memset()
+{
+  return (0);
+}
+
+int	test_puts()
+{
+  return (0);
+}
+
+int	test_strdup()
+{
+  return (0);
+}
